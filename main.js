c44947ac458a6a52bbdded3d2eb70e9f15e0011d
USER <user@USERs-MacBook-Air.local>
2023-09-25 13:33:18 +0100

    my first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+   var colours= ["green", "red", "blue"]
+   
+   
+   function colour(x){
+    document.getElementById("screen").style.backgroundColor= colours[x]
+document.getElementById("screen").innerHTML=colours[x]
+
+    }
+
+
